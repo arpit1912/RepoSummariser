06a88a8b729e911b7c591f80d4ce30e5a1208103
arpit1912 <arpitbandejiya@gmail.com>
2021-03-22 19:22:09 +0530

    added the commit data pulling functions as well as structuring of data

--- main.py
+++ main.py
@@ -143,27 +143,125 @@ class RepoSummariser:
         repos = []
         with open(f"data/{self.repo}_all_users_filtered_repo.json",) as inpFile:
             repos = json.load(inpFile)
+        #pprint(repos)
+        DATA = {}
+        for user,repos_data in repos.items():
+            data = {}
+            for repo_data in repos_data:
+                
+                owner,repo = repo_data['full_name'].split('/')
+                print(user,owner,repo)
+                count = 2
+                query_url = f"https://api.github.com/repos/{owner}/{repo}/commits?page=1&per_page=10000"
+                params = {
+                    "state": "open",
+                }
+                headers = {'Authorization': f'token {self.token}'}
+                r = requests.get(query_url, headers=headers, params=params)
+                data[f"{owner}_{repo}"] = r.json()
+    
+                while(len(r.json())!=0):
+                    query_url = f"https://api.github.com/repos/{owner}/{repo}/commits?page={count}&per_page=10000"
+                    params = {
+                            "state": "open",
+                            }
+                    headers = {'Authorization': f'token {self.token}'}
+                    r = requests.get(query_url, headers=headers, params=params)
+                    data[f"{owner}_{repo}"] = data[f"{owner}_{repo}"] + r.json()
+                    count = count + 1
+            DATA[f"{user}"] = data
+                
+        with open(f"data/{self.repo}_users_repo_commits.json","w") as outfile:
+            json.dump(DATA,outfile,indent=4)
+    
+    def filtered_commits(self):
+        data = {}
+        print(f"data/{self.repo}_users_repo_commits.json")
+        with open(f"data/{self.repo}_users_repo_commits.json",) as inpFile:
+            data = json.load(inpFile)
         
-        for user,repo_data in repos.items():
-            owner, repo = repo_data[0]['full_name'].split("/") # find the owner and name 
-            query_url = f"https://api.github.com/repos/{repo}/{owner}/pulls/"
-            print(query_url)
-            params = {
-            "author": user,
-            }
-            headers = {'Authorization': f'token {self.token}'}
-            r = requests.get(query_url, headers=headers)
-
-            print(r.json())
-            #print(repo)
-            break
+        new_data = {}
+        for user,repos in data.items():
+            temp = {}
+            for repo_name,commits in repos.items():
+                usefull_commits = []
+                #total_commits = len(repo)
+                for commit in commits:
+                    #print(commit["author"])
+                    if commit["author"] != None and commit["author"]["login"] == user:
+                        print(user,commit["author"]["login"],usefull_commits)
+                        usefull_commits.append(commit["sha"])
+                    
+                temp[repo_name] = usefull_commits
+                
+            new_data[user] = temp
+        with open(f"data/{self.repo}_users_repo_sha_usefull_commits.json","w") as outfile:
+            json.dump(new_data,outfile,indent=4)
+    
+    def commit_sha_exploration(self):
+        data = {}
+        with open(f"data/{self.repo}_users_repo_sha_usefull_commits.json",) as Inpfile:
+            data = json.load(Inpfile)
         
+        user_data = {}
+        for user,repos_dict in data.items():
+            repo_data = {}
+            
+            for repo_name, shas in repos_dict.items():
+            
+                total_add = 0
+                total_del = 0
+                total_files = []
+                
+                owner = repo_name.split("_")[0]
+                repo = repo_name[len(owner)+1:]
+                print(owner,repo)
+                
+                for sha in shas:
+                    
+                    query_url = f"https://api.github.com/repos/{owner}/{repo}/commits/{sha}"
+                    params = {
+                            "state": "open",
+                            }
+                    headers = {'Authorization': f'token {self.token}'}
+                    print(query_url)
+                    r = requests.get(query_url, headers=headers, params=params)
+                    temp = r.json()
+                    #print("soomgortm",temp["files"])
+                    total_add += temp["stats"]["additions"]
+                    total_del += temp["stats"]["deletions"]
+                    files = temp["files"]
+                    for file in files:
+                        temp_file = {}
+                        for key,val in file.items():
+                            if key == "filename" or key == "status" or key == "additions" or key == "deletions":
+                                temp_file[key] = val
+                        #print(temp_file)
+                        total_files.append(temp_file)
+                        #print(total_files)
+                        
+                    #print (sha)
+                repo_data[repo_name] = {"additions": total_add,"deletions": total_del,"files":total_files}
+            user_data[user] = repo_data
+        
+        with open(f"data/{self.repo}_users_repo_files_data.json","w") as outfile:
+            json.dump(user_data,outfile,indent=4)
+        
+    def rate_check(self):
+        query_url = f"https://api.github.com/rate_limit"
+        print(query_url)
+        headers = {'Authorization': f'token {self.token}'}
+        r = requests.get(query_url, headers=headers)
+        pprint(r.json())
         #pprint(repos)
                  
-classObject = RepoSummariser("token")
+classObject = RepoSummariser("<token>")
 classObject.initialise_repo("arpit1912","SE-gamedev")
 #classObject.get_contributors_list()
 #lassObject.get_contributor_login()        
 #classObject.get_all_user_repos()
 #classObject.filter_valid_repos()
-classObject.RepoExploration()
+#classObject.RepoExploration()
+#classObject.filtered_commits()
+#classObject.rate_check()
+classObject.commit_sha_exploration()
