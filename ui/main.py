38ea540329c8a575cac0d0645361ef7e6e0894e7
arpit1912 <arpitbandejiya@gmail.com>
2021-03-24 20:01:11 +0530

    integrated the UI and the tool class

--- ui/main.py
+++ ui/main.py
@@ -18,7 +18,14 @@ copies or substantial portions of the Software.
 Edited by Shreyansh Mehra and Arpit Bandejiya in accordance with above licenses
 """
 import os, sys
+from github import Github       # The Github Api Library
+import requests                 # for Request Handling
+import json                     # for Data handling
+import mimetypes                # for filtering the files
+import re                       # for writing Regular Expressions
+from pprint import pprint
 
+from tool import RepoSummariser
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append("../../atlastk")
 
@@ -31,14 +38,21 @@ except:
 
 fields = []
 
+obj = RepoSummariser("9ab3c7cf2a89afde31a4a30dec51c7bbaf2de1f8")
+obj.rate_check()
+
 def ac_connect(dom):
   dom.inner("", open( "Main.html").read() )
   dom.focus( "token")
 
 def ac_submit(dom):
-  reponame = dom.get_value("reponame")
-  username = dom.get_value("username")
+  username = dom.get_value("reponame")
+  reponame = dom.get_value("username")
   token = dom.get_value("token")
+  print(reponame,username,token)
+  obj = RepoSummariser(token)
+  obj.initialise_repo(username,reponame)
+  obj.start_processing()
   dom.alert("Hello, {}!".format(reponame+username+token))
   dom.focus( "token")
 
